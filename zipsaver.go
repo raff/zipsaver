680b5596c062a2f747c5ea6472c474000e30d096
Raffaele Sena <raff367@gmail.com>
2015-09-14 08:53:33 -0700

    some cleanup in preparation of adding "unzip" modes (view or extract)

--- zipsaver.go
+++ zipsaver.go
@@ -17,26 +17,11 @@ import (
 // from archive/zip struct.go
 
 const (
-	fileHeaderSignature      = 0x04034b50
-	directoryHeaderSignature = 0x02014b50
-	directoryEndSignature    = 0x06054b50
-	directory64LocSignature  = 0x07064b50
-	directory64EndSignature  = 0x06064b50
-	dataDescriptorSignature  = 0x08074b50 // de-facto standard; required by OS X Finder
-	fileHeaderLen            = 30         // + filename + extra
-	directoryHeaderLen       = 46         // + filename + extra + comment
-	directoryEndLen          = 22         // + comment
-	dataDescriptorLen        = 16         // four uint32: descriptor signature, crc32, compressed size, size
-	dataDescriptor64Len      = 24         // descriptor with 8 byte sizes
-	directory64LocLen        = 20         //
-	directory64EndLen        = 56         // + extra
-
-	// Constants for the first byte in CreatorVersion
-	creatorFAT    = 0
-	creatorUnix   = 3
-	creatorNTFS   = 11
-	creatorVFAT   = 14
-	creatorMacOSX = 19
+	fileHeaderSignature     = 0x04034b50
+	dataDescriptorSignature = 0x08074b50 // de-facto standard; required by OS X Finder
+	fileHeaderLen           = 30         // + filename + extra
+	dataDescriptorLen       = 16         // four uint32: descriptor signature, crc32, compressed size, size
+	dataDescriptor64Len     = 24         // descriptor with 8 byte sizes
 
 	// version numbers
 	zipVersion20 = 20 // 2.0
@@ -65,6 +50,7 @@ func (b *readBuf) uint64() uint64 {
 
 func main() {
 	debug := flag.Bool("debug", false, "print debug info")
+	//view := flag.Bool("v", false, "view list")
 
 	flag.Parse()
 
@@ -72,7 +58,9 @@ func main() {
 		log.Fatal("usage: ", os.Args[0], " {zip-file}")
 	}
 
-	f, err := os.Open(flag.Arg(0))
+	zipfile := flag.Arg(0)
+
+	f, err := os.Open(zipfile)
 	if err != nil {
 		log.Fatal("open ", err)
 	}
@@ -96,31 +84,43 @@ func main() {
 
 		b := readBuf(fh[:])
 		magic := b.uint32()
+		version := b.uint16()
+		flags := b.uint16()
+		comp := b.uint16()
+		ctime := b.uint16()
+		cdate := b.uint16()
+		crc32 := b.uint32()
+		clen := b.uint32()
+		ulen := b.uint32()
+		flen := b.uint16()
+		elen := b.uint16()
+
 		if magic != fileHeaderSignature {
 			log.Fatal("invalid file header signature ", magic)
 		}
 
-		fmt.Printf("magic   %08x\n", magic)
-		fmt.Printf("version %04x\n", b.uint16())
-		flags := b.uint16()
-		comp := b.uint16()
-		fmt.Printf("flags   %04x\n", flags)
-		fmt.Printf("comp    %04x\n", comp)
-		fmt.Printf("time    %04x\n", b.uint16())
-		fmt.Printf("date    %04x\n", b.uint16())
-		fmt.Printf("crc32   %08x\n", b.uint32())
-		clen := b.uint32()
-		ulen := b.uint32()
-		fmt.Printf("compressed size   %d\n", clen)
-		fmt.Printf("uncompressed size %d\n", ulen)
-		flen := b.uint16()
-		elen := b.uint16()
-		fmt.Printf("filename length   %d\n", flen)
-		fmt.Printf("extra length      %d\n", elen)
+		if *debug {
+			fmt.Printf("magic   %08x\n", magic)
+			fmt.Printf("version %04x\n", version)
+			fmt.Printf("flags   %04x\n", flags)
+			fmt.Printf("comp    %04x\n", comp)
+			fmt.Printf("time    %04x\n", ctime)
+			fmt.Printf("date    %04x\n", cdate)
+			fmt.Printf("crc32   %08x\n", crc32)
+			fmt.Printf("compressed size   %d\n", clen)
+			fmt.Printf("uncompressed size %d\n", ulen)
+			fmt.Printf("filename length   %d\n", flen)
+			fmt.Printf("extra length      %d\n", elen)
+		}
 
 		fn := make([]byte, flen)
 		if _, err := io.ReadFull(r, fn); err != nil {
 			log.Fatal("read file name ", err)
+		}
+
+		if *debug {
+			fmt.Println()
+			fmt.Println("filename", string(fn))
 		}
 
 		if elen > 0 {
@@ -129,10 +129,8 @@ func main() {
 			}
 		}
 
-		fmt.Println()
-		fmt.Println("filename", string(fn))
-
-		if comp == zip.Deflate {
+		switch comp {
+		case zip.Deflate:
 			dec := flate.NewReader(r)
 			n, err := io.Copy(ioutil.Discard, dec)
 			fmt.Println("decoded", n, "bytes")
@@ -141,14 +139,20 @@ func main() {
 			} else {
 				dec.Close()
 			}
-		} else if ulen > 0 {
-			n, err := io.CopyN(ioutil.Discard, r, int64(ulen))
-			fmt.Println("read", n, "bytes")
-			if err != nil {
-				log.Fatal("read file ", err)
+
+		case zip.Store:
+			if ulen > 0 {
+				n, err := io.CopyN(ioutil.Discard, r, int64(ulen))
+				fmt.Println("read", n, "bytes")
+				if err != nil {
+					log.Fatal("read file ", err)
+				}
+			} else {
+				log.Fatal("missing lenght")
 			}
-		} else {
-			log.Fatal("missing lenght")
+
+		default:
+			log.Fatal("unsupported compression mode ", comp)
 		}
 
 		if (flags & 0x08) != 0 {
@@ -163,14 +167,20 @@ func main() {
 			fmt.Println()
 
 			magic := b.uint32()
+			crc32 = b.uint32()
+			clen = b.uint32()
+			ulen = b.uint32()
+
 			if magic != dataDescriptorSignature {
 				log.Fatal("invalid data descriptor signature ", magic)
 			}
 
-			fmt.Printf("magic   %08x\n", magic)
-			fmt.Printf("crc32   %08x\n", b.uint32())
-			fmt.Printf("compressed size   %d\n", b.uint32())
-			fmt.Printf("uncompressed size %d\n", b.uint32())
+			if *debug {
+				fmt.Printf("magic   %08x\n", magic)
+				fmt.Printf("crc32   %08x\n", crc32)
+				fmt.Printf("compressed size   %d\n", clen)
+				fmt.Printf("uncompressed size %d\n", ulen)
+			}
 		}
 	}
 }
